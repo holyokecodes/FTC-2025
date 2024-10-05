b572fd698e57feda3881d20d9e9dd0780c854836
Sawyer Perreault <121728792+GeicoLizard111@users.noreply.github.com>
2024-10-05 12:50:30 -0400

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2025.java
@@ -2,8 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 @TeleOp(name = "Teleop2025")
 public class Teleop2025 extends LinearOpMode {
@@ -11,6 +14,9 @@ public class Teleop2025 extends LinearOpMode {
     private DcMotor leftMotor;
     private DcMotor rightMotor;
     private DcMotor armMotor;
+
+    private Servo wristServo;
+    private CRServo intakeServo;
 
     private double power = 1.0;
 
@@ -29,6 +35,12 @@ public class Teleop2025 extends LinearOpMode {
         armMotor.setTargetPosition(armMotor.getCurrentPosition());
 
 
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+
+
+
+
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -37,8 +49,8 @@ public class Teleop2025 extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            double leftPower = gamepad1.left_stick_y * power;
-            double rightPower = -gamepad1.right_stick_y * power;
+            double leftPower = -gamepad1.left_stick_y * power;
+            double rightPower = gamepad1.right_stick_y * power;
 
             leftMotor.setPower(leftPower);
             rightMotor.setPower(rightPower);
@@ -50,13 +62,36 @@ public class Teleop2025 extends LinearOpMode {
                 armMotor.setTargetPosition(armMotor.getTargetPosition() - 5);
             }
 
+            if(gamepad1.right_trigger > 0.1) {
+                intakeServo.setPower(1.0);
+            } else if (gamepad1.left_trigger > 0.1) {
+                intakeServo.setPower(-1.0);
+            } else {
+                intakeServo.setPower(0);
+            }
+
+            if(gamepad1.right_bumper) {
+                wristServo.setPosition(wristServo.getPosition() + 5);
+            }
+
+            if(gamepad1.left_bumper) {
+                wristServo.setPosition(wristServo.getPosition() - 5);
+            }
+
+//            if (wristServo.getPosition() > 150) {
+//                wristServo.setPosition(150);
+//            }
+//            if (wristServo.getPosition() < 0) {
+//                wristServo.setPosition(0);
+//            }
+
             ((DcMotorEx) armMotor).setVelocity(2100);
             armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            telemetry.addData("Left Power", leftPower);
-            telemetry.addData("Right Power", rightPower);
-            telemetry.addData("Target Position", armMotor.getTargetPosition());
-            telemetry.addData("Arm Position", armMotor.getCurrentPosition());
+
+//            telemetry.addData("Target Position", armMotor.getTargetPosition());
+//            telemetry.addData("Arm Position", armMotor.getCurrentPosition());
+            telemetry.addData("Wrist Position", wristServo.getPosition());
             telemetry.update();
         }
     }
