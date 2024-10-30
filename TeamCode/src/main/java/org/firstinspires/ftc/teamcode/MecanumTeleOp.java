1a4434f6e46243b43abd5f976efeb2a4178d6329
Sawyer Perreault <121728792+GeicoLizard111@users.noreply.github.com>
2024-10-30 19:47:22 -0400

    added ftc lib, control updateees

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,41 +1,79 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+
+enum Direction {
+    Still,
+    Intake,
+    Outtake,
+}
 
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
 
-    static double speedMultiplier = 1;
-
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor armMotor;
-        DcMotor longArmMotor;
+        boolean isSlowMode = false;
 
-        Servo wristServo;
+        Direction intakeStatus = Direction.Still;
+
+//        boolean bucketFlipped = false;
+
+
+//        Servo bucketServo;
         CRServo intakeServo;
+        Servo wristServo;
 
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
-        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armMotor.setTargetPosition(armMotor.getCurrentPosition());
+        Motor armMotor = new Motor(hardwareMap, "armMotor");
 
-        longArmMotor = hardwareMap.dcMotor.get("longArmMotor");
-        longArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        longArmMotor.setTargetPosition(armMotor.getCurrentPosition());
+        armMotor.setRunMode(Motor.RunMode.PositionControl);
+        armMotor.setPositionCoefficient(0.05);
+        armMotor.setPositionTolerance(3);
+        armMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        armMotor.resetEncoder();
+
+        int armMotorTargetPosition = 0;
+//
+//        Motor longArmMotor = new Motor(hardwareMap, "longArmMotor");
+//
+//        longArmMotor.setRunMode(Motor.RunMode.PositionControl);
+//        longArmMotor.setPositionCoefficient(0.05);
+//        longArmMotor.setPositionTolerance(3);
+
+
+
 
         // Init Servos
-        wristServo = hardwareMap.get(Servo.class, "wristServo");
+//        bucketServo = hardwareMap.get(Servo.class, "bucketServo");
         intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+
+        GamepadEx controller1 = new GamepadEx(gamepad1);
+
+        ButtonReader aButton = new ButtonReader(controller1, GamepadKeys.Button.A);
+        ButtonReader bButton = new ButtonReader(controller1, GamepadKeys.Button.B);
+        ButtonReader yButton = new ButtonReader(controller1, GamepadKeys.Button.Y);
+        ButtonReader xButton = new ButtonReader(controller1, GamepadKeys.Button.X);
+        ButtonReader leftBumper = new ButtonReader(controller1, GamepadKeys.Button.LEFT_BUMPER);
+        ButtonReader rightBumper = new ButtonReader(controller1, GamepadKeys.Button.RIGHT_BUMPER);
+
+        TriggerReader leftTrigger = new TriggerReader(controller1, GamepadKeys.Trigger.LEFT_TRIGGER);
+        TriggerReader rightTrigger = new TriggerReader(controller1, GamepadKeys.Trigger.RIGHT_TRIGGER);
+
 
         waitForStart();
 
@@ -54,51 +92,105 @@ public class MecanumTeleOp extends LinearOpMode {
             double backLeftPower = robotPower * Math.sin(robotAngle) + rightX;
             double backRightPower = robotPower * Math.cos(robotAngle) - rightX;
 
+            double speedMultiplier = isSlowMode ? 0.25 : 1;
+
             backLeftMotor.setPower(-backLeftPower * speedMultiplier);
             backRightMotor.setPower(backRightPower * speedMultiplier);
             frontLeftMotor.setPower(-frontLeftPower * speedMultiplier);
             frontRightMotor.setPower(frontRightPower * speedMultiplier);
 
+            yButton.readValue();
+            aButton.readValue();
+            bButton.readValue();
+            xButton.readValue();
+
+            rightBumper.readValue();
+            leftBumper.readValue();
+
+            rightTrigger.readValue();
+            leftTrigger.readValue();
 
 
-            if(gamepad1.a) {
-                armMotor.setTargetPosition(armMotor.getTargetPosition() + 5);
-            }
-            if(gamepad1.b) {
-                armMotor.setTargetPosition(armMotor.getTargetPosition() - 5);
-            }
+            // Y toggle slow mode
+            // arm triggers up and down
+            // intake A and B
+            // X is toggle bucket
+            // linear arm go up and down bumpers
+            // dpad is shortcuts
 
-            if(gamepad1.right_trigger > 0.1) {
-                intakeServo.setPower(1.0);
-            } else if (gamepad1.left_trigger > 0.1) {
-                intakeServo.setPower(-1.0);
-            } else {
-                intakeServo.setPower(0);
-            }
 
-            if(gamepad1.right_bumper) {
-                wristServo.setPosition(wristServo.getPosition() + 1);
-            }
-
-            if(gamepad1.left_bumper) {
-                wristServo.setPosition(wristServo.getPosition() - 1);
-            }
-
-            if (gamepad1.dpad_up) {
-                longArmMotor.setTargetPosition(longArmMotor.getTargetPosition() + 5);
+            // slow mode
+            if (yButton.wasJustReleased()) {
+                isSlowMode = !isSlowMode;
             }
 
-            if (gamepad2.dpad_down) {
-                longArmMotor.setTargetPosition(longArmMotor.getTargetPosition() - 5);
+            if (rightTrigger.isDown()) {
+                armMotorTargetPosition += 5;
+            }
+            if (leftTrigger.isDown()) {
+                armMotorTargetPosition -= 5;
             }
 
-            ((DcMotorEx) armMotor).setVelocity(2100);
-            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            if (aButton.wasJustReleased()) {
+                if(intakeStatus == Direction.Intake) {
+                    intakeStatus = Direction.Still;
+                } else {
+                    intakeStatus = Direction.Intake;
+                }
+            }
 
-            ((DcMotorEx) longArmMotor).setVelocity(2100);
-            longArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            telemetry.addData("long arm position", longArmMotor.getCurrentPosition());
-            telemetry.addData("long arm target position", longArmMotor.getTargetPosition());
+            if (bButton.wasJustReleased()) {
+                if(intakeStatus == Direction.Outtake) {
+                    intakeStatus = Direction.Still;
+                } else {
+                    intakeStatus = Direction.Outtake;
+                }
+            }
+
+            switch (intakeStatus) {
+                case Intake:
+                    intakeServo.setPower(-1);
+                    break;
+                case Outtake:
+                    intakeServo.setPower(1);
+                    break;
+                default:
+                    intakeServo.setPower(0);
+            }
+
+            if (armMotor.getCurrentPosition() < 600) {
+                wristServo.setPosition(60);
+            } else if (armMotor.getCurrentPosition() > 600 && armMotor.getCurrentPosition() < 1200) {
+                wristServo.setPosition(0);
+            } else if (armMotor.getCurrentPosition() > 1200) {
+                wristServo.setPosition(180);
+            }
+
+
+
+//            if (xButton.wasJustReleased()) {
+//                bucketFlipped = !bucketFlipped;
+//            }
+
+//            bucketServo.setPosition(bucketFlipped ? 0 : 180);
+
+//            if (leftBumper.isDown()) {
+//                longArmMotor.setTargetPosition(longArmMotor.getCurrentPosition() + 5);
+//            }
+//
+//            if (rightTrigger.isDown()) {
+//                longArmMotor.setTargetPosition(longArmMotor.getCurrentPosition() - 5);
+//            }
+
+
+            armMotor.set(0.75);
+
+            armMotor.setTargetPosition(armMotorTargetPosition);
+
+            telemetry.addData("Arm Position", armMotor.getCurrentPosition());
+            telemetry.addData("Arm Target Position", armMotorTargetPosition);
+
+            telemetry.update();
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop2025.java
@@ -1,101 +1,88 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.IMU;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-
-@TeleOp(name = "Teleop2025")
+@TeleOp
 public class Teleop2025 extends LinearOpMode {
 
-    DcMotor frontLeftMotor;
-    DcMotor backLeftMotor;
-    DcMotor frontRightMotor;
-    DcMotor backRightMotor;
-//    DcMotor armMotor;
-//
-//    Servo wristServo;
-//    CRServo intakeServo;
-
-//    IMU imu;
-
+    static double speedMultiplier = 1;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+        DcMotor armMotor;
 
-        // Init Motors
-        frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-//        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-//        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        armMotor.setTargetPosition(armMotor.getCurrentPosition());
-//
-//        // Init Servos
-//        wristServo = hardwareMap.get(Servo.class, "wristServo");
-//        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
+        Servo wristServo;
+        CRServo intakeServo;
 
-        // Init IMU
-//        imu = hardwareMap.get(IMU.class, "imu");
-//        imu.initialize(
-//                new IMU.Parameters(
-//                        new RevHubOrientationOnRobot(
-//                                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-//                                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
-//                        )
-//                )
-//        );
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setTargetPosition(armMotor.getCurrentPosition());
 
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        // Init Servos
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+        intakeServo = hardwareMap.get(CRServo.class, "intakeServo");
 
         waitForStart();
 
+        if (isStopRequested()) return;
+
         while (opModeIsActive()) {
+            double leftX = -gamepad1.left_stick_x;
+            double leftY = gamepad1.left_stick_y;
+            double rightX = gamepad1.right_stick_x;
 
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+            double robotPower = Math.hypot(leftX, leftY);
+            double robotAngle = Math.atan2(leftY, leftX) - Math.toRadians(45);
 
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-//            if (gamepad1.options) {
-//                imu.resetYaw();
-//            }
+            double frontLeftPower = robotPower * Math.cos(robotAngle) + rightX;
+            double frontRightPower = robotPower * Math.sin(robotAngle) - rightX;
+            double backLeftPower = robotPower * Math.sin(robotAngle) + rightX;
+            double backRightPower = robotPower * Math.cos(robotAngle) - rightX;
 
-//            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            backLeftMotor.setPower(-backLeftPower * speedMultiplier);
+            backRightMotor.setPower(backRightPower * speedMultiplier);
+            frontLeftMotor.setPower(-frontLeftPower * speedMultiplier);
+            frontRightMotor.setPower(frontRightPower * speedMultiplier);
 
-//            // Rotate the movement direction counter to the bot's rotation
-//            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-//            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-//            rotX = rotX * 1.1;  // Counteract imperfect strafing
-//
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-//            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-//            double frontLeftPower = (rotY + rotX + rx) / denominator;
-//            double backLeftPower = (rotY - rotX + rx) / denominator;
-//            double frontRightPower = (rotY - rotX - rx) / denominator;
-//            double backRightPower = (rotY + rotX - rx) / denominator;
-//
-////            frontLeftMotor.setPower(frontLeftPower);
-////            backLeftMotor.setPower(backLeftPower);
-////            frontRightMotor.setPower(frontRightPower);
-//            backRightMotor.setPower(backRightPower);
-////            telemetry.addData("Wrist Position", wristServo.getPosition());
-////            telemetry.update();
+
+            if(gamepad1.a) {
+                armMotor.setTargetPosition(armMotor.getTargetPosition() + 5);
+            }
+            if(gamepad1.b) {
+                armMotor.setTargetPosition(armMotor.getTargetPosition() - 5);
+            }
+
+            if(gamepad1.right_trigger > 0.1) {
+                intakeServo.setPower(1.0);
+            } else if (gamepad1.left_trigger > 0.1) {
+                intakeServo.setPower(-1.0);
+            } else {
+                intakeServo.setPower(0);
+            }
+
+            if(gamepad1.right_bumper) {
+                wristServo.setPosition(wristServo.getPosition() + 1);
+            }
+
+            if(gamepad1.left_bumper) {
+                wristServo.setPosition(wristServo.getPosition() - 1);
+            }
+
+            ((DcMotorEx) armMotor).setVelocity(2100);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         }
     }
 }
